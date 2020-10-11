9ab72df79d87c870703d59aec103ddf6b77aeee3
Allen Richard Lea <allenlea2007@gmail.com>
2020-10-11 18:32:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
 min_value = 999999
- name_hash.each do |element|
+ name_hash.each do |key, value|
     if element < min_value
       min_value = element
     end  
